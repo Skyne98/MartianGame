152bbb0f4cb4636c578483b6a77667c73929f3c2
Aleksey Halahan <ahalahan@gmail.com>
2016-12-10 16:10:28 +0100

    Added IInput interface and temporary mouse implementation

---
+++ MartianGame/Assets/Scripts/IInput.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public interface IInput
+    {
+        Vector2 GetArmPosition(Arm arm);
+        bool GetArmPressed(Arm arm); 
+
+        Action<ScaleGestureArgs> OnScaleGesture { get; set; }
+        Action<ArmDownGestureArgs> OnArmDownGesture { get; set; }
+        Action<ArmGrabbedGestureArgs> OnArmGrabbedGesture { get; set; }
+        Action<ArmReleasedGestureArgs> OnArmReleasedGesture { get; set; }
+    }
+
+    public class ScaleGestureArgs
+    {
+        public float Value; //0 - Arms together 1 - Arms are fully stretched 
+    }
+
+    public class ArmDownGestureArgs
+    {
+        public Arm Arm;
+    }
+
+    public class ArmGrabbedGestureArgs
+    {
+        public Arm Arm;
+        public Vector2 Position;
+    }
+
+    public class ArmReleasedGestureArgs
+    {
+        public Arm Arm;
+        public Vector2 Position;
+    }
+
+    public enum Arm
+    {
+        Left,
+        Right
+    }
+}
---
+++ MartianGame/Assets/Scripts/MouseKeyboardInput.cs
@@ -0,0 +1,80 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace MartianGame.Scripts
+{
+    public class MouseKeyboardInput : MonoBehaviour, IInput
+    {
+        public Action<ArmDownGestureArgs> OnArmDownGesture
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Action<ArmGrabbedGestureArgs> OnArmGrabbedGesture
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Action<ArmReleasedGestureArgs> OnArmReleasedGesture
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Action<ScaleGestureArgs> OnScaleGesture
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Vector2 GetArmPosition(Arm arm)
+        {
+            return Input.mousePosition;
+        }
+
+        public bool GetArmPressed(Arm arm)
+        {
+            if (arm == Arm.Left)
+            {
+                return Input.GetMouseButton(0);
+            }
+            else
+            {
+                return Input.GetMouseButton(1);
+            }
+        }
+    }
+}
